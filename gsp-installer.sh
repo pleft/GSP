8b783cbe8d12f47e04099f5dcfdae9905b105ae6
Panos L <panos.lefteratos@gmail.com>
2018-08-15 17:10:28 +0300

    Update gsp-installer.sh

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -3,15 +3,15 @@ echo $'\n******* 1) Installing NWjs...'
 wget https://github.com/LeonardLaszlo/nw.js-armv7-binaries/releases/download/v0.27.6/nwjs-sdk-v0.27.6-linux-arm.tar.gz
 tar -xvf nwjs-sdk-v0.27.6-linux-arm.tar.gz
 sudo cp nwjs-sdk-v0.27.6-linux-arm/lib/*.so /usr/lib
-echo $'\n******* 2) Updating GSP, any user changes will be lost!'
+echo $'\n******* 2) Updating GSP, any local changes will be lost!'
 git fetch --all
 git reset --hard origin/master
-echo $'\n******* 2) Creating shortcuts'
+echo $'\n******* 3) Creating shortcuts'
 echo /home/cpi/GSP/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 > /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
 cp ./common/GSPLauncher.png /home/cpi/apps/launcher/skin/default/Menu/GameShell/GSPLauncher.png
-echo $'\n******* 5) removing archives...'
+echo $'\n******* 4) removing archives...'
 rm *.tar.gz
 echo "***********************************************************************"
 echo "* If no errors, reboot your GameShell and select the GSPLauncher icon *" 
-echo "*                     AND HAVE FUN!                                   *"
+echo "*                      HAVE FUN!                                      *"
 echo "***********************************************************************"
