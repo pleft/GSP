b4509de6f63591ddabb5855803603f5ef8e330ad
Panos L <panos.lefteratos@gmail.com>
2018-08-16 10:13:42 +0300

    Making GSPLauncher.sh executable

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -8,6 +8,7 @@ git fetch --all
 git reset --hard origin/master
 echo $'\n******* 3) Creating shortcuts'
 echo /home/cpi/GSP/nwjs-sdk-v0.27.6-linux-arm/nw /home/cpi/GSP --use-gl=egl --ignore-gpu-blacklist --disable-accelerated-2d-canvas --num-raster-threads=2 > /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
+chmod +x /home/cpi/apps/launcher/Menu/GameShell/GSPLauncher.sh
 cp ./common/GSPLauncher.png /home/cpi/apps/launcher/skin/default/Menu/GameShell/GSPLauncher.png
 echo $'\n******* 4) removing archives...'
 rm *.tar.gz
