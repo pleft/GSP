f6c666da0c3906ed9b88990fd3b263d495e20eaa
Panos L <panos.lefteratos@gmail.com>
2018-08-20 12:02:19 +0300

    download and install libnss3-dev. Closes #3

--- gsp-installer.sh
+++ gsp-installer.sh
@@ -3,6 +3,8 @@ echo $'\n******* 1) Installing NWjs...'
 wget https://github.com/LeonardLaszlo/nw.js-armv7-binaries/releases/download/v0.27.6/nwjs-sdk-v0.27.6-linux-arm.tar.gz
 tar -xvf nwjs-sdk-v0.27.6-linux-arm.tar.gz
 sudo cp nwjs-sdk-v0.27.6-linux-arm/lib/*.so /usr/lib
+sudo apt-get update
+sudo apt-get install libnss3-dev
 echo $'\n******* 2) Updating GSP, any local changes will be lost!'
 git fetch --all
 git reset --hard origin/master
